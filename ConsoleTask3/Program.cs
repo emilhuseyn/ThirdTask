7655d96127e61c8015b503b734135837b74f7a27
emilhuseyn <145687752+emilhuseyn@users.noreply.github.com>
2023-09-28 23:24:49 +0400

    Update Program.cs

--- ConsoleTask3/Program.cs
+++ ConsoleTask3/Program.cs
@@ -1,30 +1,30 @@
 ﻿#region task 1
-//int k = 1;
-//for(int i = 1; i <= 5; i++)
-//{
-//    Console.WriteLine(i*i);
-//}
+int k = 1;
+for(int i = 1; i <= 5; i++)
+{
+    Console.WriteLine(i*i);
+}
 #endregion
 #region task 2
-//int sum = 0;
-//int[] array = { 4, 23, 13, 10, 12, 5, 9, 32 };
-//for(int i = 0; i < array.Length; i++)
-//{
-//    if (array[i] % 2 == 1)
-//    {
-//        sum += array[i];
-//    }
-//}
-//Console.WriteLine(sum); 
+int sum = 0;
+int[] array = { 4, 23, 13, 10, 12, 5, 9, 32 };
+for(int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 1)
+    {
+        sum += array[i];
+    }
+}
+Console.WriteLine(sum); 
 #endregion
 #region task 3
-//int cem = 0;
-//int[] arr = { 2, 41, 23, 41, 32, 91, 5, 67, 94 };
-//for(int j = 1; j < arr.Length; j=j+2)
-//{
-//    cem = cem + arr[j];
-//}
-//Console.WriteLine(cem);
+int cem = 0;
+int[] arr = { 2, 41, 23, 41, 32, 91, 5, 67, 94 };
+for(int j = 1; j < arr.Length; j=j+2)
+{
+    cem = cem + arr[j];
+}
+Console.WriteLine(cem);
 #endregion
 #region task 4
 bool t = true;
